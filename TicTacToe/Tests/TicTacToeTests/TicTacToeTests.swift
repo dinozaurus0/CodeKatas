1298f626a2618eab45cb54be372f36953c427993
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2024-01-21 12:30:01 +0200

    Updated the to do list for test by marking the cases that were already covered with a `++`.

--- TicTacToe/Tests/TicTacToeTests/TicTacToeTests.swift
+++ TicTacToe/Tests/TicTacToeTests/TicTacToeTests.swift
@@ -7,12 +7,12 @@ import TicTacToe
 
  General rules:
  1. There are 2 players in the game. (X & 0)
- 2. Players can specify the square in which they wanna place their character (X or 0).
+ 2. ++ Players can specify the square in which they wanna place their character (X or 0). (Covered by point 5)
  3. Each player take a turn, then is the turn of the other player.
  4. They can't override each other choices.
- 5. ++ Players can specify a cell from 1-9. Verify that all transitions are correct. Is this independent of player? <--
- 6. Players' movements are cumulative
- 7. Verify that each players choice is correct.
+ 5. ++ Players can specify a cell from 1-9. Verify that all transitions are correct. Is this independent of player?
+ 6. Players' movements are cumulative <--
+ 7. ++ Verify that each players choice is correct. (Covered by point 5 where we use any player)
 
  Winning rules:
  7. One player completes a column/row/diagonal. That player wins.
